4d013fd7b76947045b1ed82587e2ddf24094aad6
wn300 <wilmer.mancera93@gmail.com>
2018-01-03 23:35:01 -0500

    update and query in api controller and methods of service

--- src/java/apiRest/controllers/cube/CubeController.java
+++ src/java/apiRest/controllers/cube/CubeController.java
@@ -18,31 +18,43 @@ import javax.ws.rs.PathParam;
  * @author wilmer.mancera
  */
 @Path("Cube")
-public class CubeController  {
-    
+public class CubeController {
+
     @GET
     @Path("Test/x/{x}/y/{y}")
-    public String test (@PathParam("x") Integer x, @PathParam("y") Integer y){
-    
+    public String test(@PathParam("x") Integer x, @PathParam("y") Integer y) {
+
         return String.valueOf(x * y);
     }
-    
+
     @GET
     @Path("GetCube")
-    public Cube getCube (){    
+    public Cube getCube() {
         return CubeService.GetCube();
     }
-    
+
     @POST
     @Path("CreateCube/n/{n}")
-    public void createCube (@PathParam("n") Integer n){
+    public void createCube(@PathParam("n") Integer n) {
         CubeService.CreateCube(n);
     }
-    
+
     @PUT
-    @Path("UpdateCube/x/{x}/y/{y}/z/{z}/w/{w}")
-    public void updateCube (@PathParam("x") Integer x,@PathParam("y") Integer y,@PathParam("z") Integer z){
-        CubeService.UpdateCube(1,x,y,z);
+    @Path("UpdateCube/count/{count}/x/{x}/y/{y}/z/{z}/w/{w}")
+    public void updateCube(@PathParam("count") Integer count, @PathParam("x") Integer x, @PathParam("y") Integer y, @PathParam("z") Integer z, @PathParam("w") Integer w) {
+        CubeService.UpdateCube(count, x, y, z, w);
+    }   
+    
+    @POST
+    @Path("QueryCube/count/{count}/xOne/{xOne}/yOne/{yOne}/zOne/{zOne}/xTwo/{xTwo}/yTwo/{yTwo}/zTwo/{zTwo}")
+    public void queryCube(@PathParam("count") Integer count, @PathParam("xOne") Integer xOne, @PathParam("yOne") Integer yOne, @PathParam("zOne") Integer zOne, @PathParam("xTwo") Integer xTwo, @PathParam("yTwo") Integer yTwo, @PathParam("zTwo") Integer zTwo) {
+        CubeService.QueryCube(count, xOne, yOne, zOne, xTwo, yTwo, zTwo);
+    } 
+    
+    @GET
+    @Path("GetSum")
+    public Long getSum() {
+        return CubeService.GetSum();
     }
 
 }
--- src/java/core/services/cube/CubeService.java
+++ src/java/core/services/cube/CubeService.java
@@ -12,11 +12,13 @@ import core.models.Cube;
  * @author wilmer.mancera
  */
 public class CubeService {
+
+    public static long sum;
     public static Cube cube;
-    public static int[] W; 
-    
-    public CubeService(){
-        this.cube = null;
+    public static int[] W;
+
+    public CubeService() {
+        this.cube = new Cube();
     }
 
     public static Cube GetCube() {
@@ -24,8 +26,8 @@ public class CubeService {
     }
 
     public static void CreateCube(Integer N) {
-        cube = new Cube();       
-        
+        cube = new Cube();
+
         int[] n = new int[N];
 
         W = n;
@@ -33,17 +35,39 @@ public class CubeService {
         cube.setY(n);
         cube.setZ(n);
     }
-    
-    public static void UpdateCube(Integer count, Integer x, Integer y, Integer z){
+
+    public static void UpdateCube(Integer count, Integer x, Integer y, Integer z, Integer w) {
         cube.x[count] = x - 1;
         cube.y[count] = y - 1;
-        cube.z[count] = z - 1;        
-        
+        cube.z[count] = z - 1;
+        W[count] = w;
+
         for (int k = 0; k < count; k++) {
             if (cube.x[k] == cube.x[count] && cube.y[k] == cube.y[count] && cube.z[k] == cube.z[count]) {
                 W[k] = 0;
             }
         }
+    }
+
+    public static void QueryCube(Integer count, Integer xOne, Integer yOne, Integer zOne, Integer xTwo, Integer yTwo, Integer zTwo) {
+
+        sum = 0;
+        xOne = xOne - 1;
+        yOne = yOne - 1;
+        zOne = zOne - 1;
+        xTwo = xTwo - 1;
+        yTwo = yTwo - 1;
+        zTwo = zTwo - 1;
+
+        for (int a = 0; a < count; a++) {
+            if (cube.x[a] >= xOne && cube.y[a] >= yOne && cube.z[a] >= zOne && cube.x[a] <= xTwo && cube.y[a] <= yTwo && cube.z[a] <= zTwo) {
+                sum += W[a];
+            }
+        }
+    }
+    
+    public static long GetSum() {
+        return sum;
     }
 
 }
